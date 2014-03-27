0a0922563e71c1d5e3eff8e074ade2601f0b3091
Tamas Blummer <tamas@bitsofproof.com>
2014-03-27 09:38:18 +0100

    create account package

--- src/main/java/com/bitsofproof/supernode/account/KeyListAccountManager.java
+++ src/main/java/com/bitsofproof/supernode/account/KeyListAccountManager.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.bitsofproof.supernode.wallet;
+package com.bitsofproof.supernode.account;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
